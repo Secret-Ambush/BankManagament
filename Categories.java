ab8362ec5861ab4e05e43350eb0d1c3fd00f11b2
Karthik-Vishal03 <120249363+Karthik-Vishal03@users.noreply.github.com>
2022-12-11 20:39:37 +0400

    Add files via upload

---
+++ Categories.java
@@ -0,0 +1,50 @@
+package inventoryManagment;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Font;
+
+public class Categories extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Categories catframe = new Categories();
+					catframe.setSize(1000,750);
+					catframe.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Categories() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel catTitle = new JLabel("CATEGORIES");
+		catTitle.setFont(new Font("Algerian", Font.PLAIN, 30));
+		catTitle.setBounds(407,15, 186, 40);
+		contentPane.add(catTitle);
+	}
+}
